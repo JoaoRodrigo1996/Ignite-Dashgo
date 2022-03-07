bcd07057e296af383d5705f4885d7f26861ce9fb
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2022-03-07 11:40:57 -0300

    Sinalizando refeth dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -32,25 +32,31 @@ type User = {
 }
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users")
-    const data = await response.json()
+  const { data, isLoading, isFetching, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users")
+      const data = await response.json()
 
-    const users = data.users.map((user: User) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: new Date(user.created_at).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      }
-    })
+      const users = data.users.map((user: User) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          created_at: new Date(user.created_at).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        }
+      })
 
-    return users
-  })
+      return users
+    },
+    {
+      staleTime: 1000 * 5, // 5 segundos
+    }
+  )
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -68,6 +74,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
